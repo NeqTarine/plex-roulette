74263716ff751a4bbb249538af73cc429335e692
Damien <damien@hidora.io>
2024-06-17 16:17:04 +0200

    ff

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +12,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+        <Head>
+        <link
+          rel="canonical"
+          href="https://plex-roulette.com/"
+          key="canonical"
+        />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
