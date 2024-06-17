127a33082da12f38dec08e6986e17a5838663f02
Damien <damien@hidora.io>
2024-06-17 09:38:34 +0200

    Change layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,6 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="canonical" key="canonical" href="https://plex-roulette.com/" />
       <body className={inter.className}>{children}</body>
     </html>
   );
