df712061b3a007b6bdcb2e3fd966e279ce73ad41
Damien <damien@hidora.io>
2024-06-17 16:07:28 +0200

    Change URL conical

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,7 @@ import logo from "/public/logov2.webp";
 import Head from 'next/head';
 
 
+
 export default function Home() {
 
   const loginOnPlex = () => {
@@ -43,13 +44,15 @@ export default function Home() {
 
 
   return (
-    <main className="flex items-center justify-center min-h-screen flex-col p-24">
-      <div>
-      <Head>
-      <link rel="canonical" key="canonical" href="https://plex-roulette.com/" />
-      <title key="title">Plex roulette</title>
+    <div>
+        <Head>
+        <link
+          rel="canonical"
+          href="https://plex-roulette.com/"
+          key="canonical"
+        />
       </Head>
-      </div>
+    <main className="flex items-center justify-center min-h-screen flex-col p-24">
       {/* Logo en haut à gauche */}
       <div className="absolute top-0 left-10 p-4">
         <Image src={logo} alt="Logo" width={150} height={150} />
@@ -68,9 +71,8 @@ export default function Home() {
         <div className="relative place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         </div>
       </div>
-
-
     </main>
+    </div>
   );
 }
 
