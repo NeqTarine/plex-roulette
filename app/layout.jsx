9a46a53847b869474b02b589a7f3916cdcf24516
Damien <damien@hidora.io>
2024-06-17 15:56:50 +0200

    cahnge title

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Plex roulette",
-  description: "Discover cinematic surprises on Plex: let chance choose your next movie!",
+  title: "Plex roulette: Random Movies & TV Shows",
+  description: "Let random selection pick your next movie, TV show, or anime. Explore a vast library and dive into new adventures with every play. Compatible with all Plex platforms, discovering new favorites has never been easier!",
 };
 
 export default function RootLayout({ children }) {
